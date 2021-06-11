4b45cb9e9e716a87ea64d741d558beca1bf543fd
sedasha <seda_sha@yahoo.com>
2021-06-11 15:25:40 -0400

    reverse

--- src/RandomGame/Random.jsx
+++ src/RandomGame/Random.jsx
@@ -7,7 +7,7 @@ const Random =({number, times})=>{
     return (
 
         <div >
-            <div className= {times>1?"number bigger_than_one":"number"}>{number} Armen</div>
+            <div className= {times>1?"number bigger_than_one":"number"}>{number}</div>
         </div>
     )
 }
